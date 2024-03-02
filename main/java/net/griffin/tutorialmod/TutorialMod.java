49f8863a7a94e968f6a7ccbdf994f382883217cb
ricepuddin4 <74981397+ricepuddin4@users.noreply.github.com>
2024-03-02 09:09:53 -0500

    Add files via upload

---
+++ main/java/net/griffin/tutorialmod/TutorialMod.java
@@ -0,0 +1,30 @@
+package net.griffin.tutorialmod;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = TutorialMod.MODID, name = TutorialMod.NAME, version = TutorialMod.VERSION)
+public class TutorialMod
+{
+    public static final String MODID = "tutorialmod";
+    public static final String NAME = "Tutorial Mod";
+    public static final String VERSION = "1.0";
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
